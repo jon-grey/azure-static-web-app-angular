cd0fbb6142675117a28eb6f520c0900df4b596d1
Jon Grey <jon.arnold.grey@gmail.com>
2021-03-28 15:47:55 +0200

    Add service to connect to api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { AppService } from './app.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>Hello {{value}}. Payload from api/date: "{{payload}}". </div>`,
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   value = 'World';
+
+  constructor(private appService: AppService) { }
+
+  destroy$: Subject<boolean> = new Subject<boolean>();
+
+  payload: any = {};
+  counter = 0;
+
+  ngOnInit() {
+    this.getTasks();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
+
+  getTasks() {
+    this.appService.getDate()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(payload => this.payload = JSON.stringify(payload));
+  }
+
+
+
 }
